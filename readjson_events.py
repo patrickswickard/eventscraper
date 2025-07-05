fbd71df327539bdb1c895d8a16b094b6558ffe7b
Patrick Swickard <swickape@hotmail.com>
2025-07-05 13:09:34 -0400

    readjson checkin

--- readjson_events.py
+++ readjson_events.py
@@ -1,7 +1,31 @@
 """Demo to read in json file"""
 import json
+import re
 
 with open('all_events.json','r',encoding='utf-8') as myjsonfile:
-  json_hash = json.load(myjsonfile)
+  json_list = json.load(myjsonfile)
 
-print(json_hash)
+#print(json_hash)
+
+def get_all_date(thisdate):
+  for thisevent in json_list:
+    thisfulldatetime = thisevent['fulldatetime']
+    event_date_match = re.search(thisdate,thisfulldatetime)
+    if event_date_match:
+      print(thisevent)
+
+def get_all_location(thislocation):
+  for thisevent in json_list:
+    thiscanonloc = thisevent['canonical_location']
+    event_loc_match = re.search(thislocation,thiscanonloc)
+    if event_loc_match:
+      print(thisevent)
+
+#thislocation = r"Ottobar"
+#thislocation = r"Peabody Heights|No Land Beyond"
+
+#get_all_location(thislocation)
+
+
+thisdate = r"2025-07-05"
+get_all_date(thisdate)
